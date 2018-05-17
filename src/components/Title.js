034d4d44f9f9c0aa551cdc2f4f92e694254b047c
Emma Uddesson <emma.uddesson@gmail.com>
2018-05-17 20:41:18 +0200

    Add prop-types
    Install prop-types npm-package and add it to project

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function Title(props){
     return(
         <h1 className="u-center">Your trip to {props.destination}</h1>
     );
 };
+
+Title.propTypes = {
+    destination: PropTypes.string
+}
 
 export default Title;
