d60b34b4536d08d9186808a39c5b3aa8d620696d
Emma Uddesson <emma.uddesson@gmail.com>
2018-05-17 18:27:51 +0200

    Remove unused component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import EditForm from './EditForm';
 import Info from './Info';
 
 function Main(){
     return(
         <div className="main testborder">
             <Info />
-            <EditForm/>
         </div>
     )
 }
