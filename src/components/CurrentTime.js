a9fe706611f92e332d86fde9fb07e1d41c1ea9f8
Emma Uddesson <emma.uddesson@gmail.com>
2018-05-17 15:19:59 +0200

    Show current time for Stockholm + Tokyo

--- src/components/CurrentTime.js
+++ src/components/CurrentTime.js
@@ -1,8 +1,32 @@
 import React from 'react';
 
+/* TODO: Make this into a class and use state
++ componentDidMount to update time every sec */
+
 function CurrentTime(){
+
+    function setTime(){
+        let stockholmTime = new Date ();
+        let tokyoTime = new Date(stockholmTime);
+
+        // Manually set the time difference
+        tokyoTime.setHours(stockholmTime.getHours() + 7);
+
+        let currentTime = {
+            stockholm: stockholmTime.toLocaleTimeString(),
+            tokyo: tokyoTime.toLocaleTimeString()
+        }
+
+        return currentTime;
+    }
+
+    let time = setTime();
+
     return(
-        <p className="align-right">HH:MM:SS</p>
+        <div>
+            <p className="align-right">Stockholm: {time.stockholm}</p>
+            <p className="align-right">Tokyo: {time.tokyo}</p>
+        </div>
     );
 };
 
