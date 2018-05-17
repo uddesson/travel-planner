c4444d020764bc3ed184d80bce25287b00daec87
Emma Uddesson <emma.uddesson@gmail.com>
2018-05-17 12:20:43 +0200

    Fetch and display weather-data
    Add credits to Open Weather Map in README

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # TBA
 
 Created with [Create React App](https://github.com/facebookincubator/create-react-app).
+
+## Resources
+
+* [Open Weather Map](https://openweathermap.org/)
--- src/components/Temperature.js
+++ src/components/Temperature.js
@@ -1,10 +1,35 @@
 import React from 'react';
 
 class Temperature extends React.Component{
+
+    state = {
+        temperature: '',
+        description: ''
+      }
+
+    componentDidMount(){
+        this.fetchWeather();
+    }
+
+    fetchWeather = () => {
+        const key = '4bfbe202a7989b703e63a5a52ba0c29a';
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=Tokyo&units=metric&lang=EN&APPID=${key}`)
+            .then(response => response.json())
+            .then((weather) => {
+                let weatherDescription = weather.weather[0].description;
+                let temperatureToRound = weather.main.temp;
+                let averageTemp = Math.round(temperatureToRound);
+
+                this.setState({temperature: averageTemp, description: weatherDescription})
+            })
+    }
+
     render(){
         return(
             <div className="testborder">
-                Temperature
+                { this.state.temperature}
+                <br/>
+                {this.state.description }
             </div>
         );
     }
