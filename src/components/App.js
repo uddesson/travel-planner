6d366266ccd1d67bd5ee44440981ea3fd8ab5e9c
Emma Uddesson <emma.uddesson@gmail.com>
2018-05-18 11:04:46 +0200

    Fix wrong prop name

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   render() {
     return (
       <div className="container testborder">
-        <Header sayHey={this.testClick}/>
+        <Header testClick={this.testClick}/>
         <Main />
       </div>
     );
