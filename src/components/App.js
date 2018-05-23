ef8604dd3df78756ee2e8c91d0abf5588bc56ad3
Emma Uddesson <emma.uddesson@gmail.com>
2018-05-23 16:06:52 +0200

    Store Options set by user with localStorage

--- src/components/App.js
+++ src/components/App.js
@@ -6,18 +6,44 @@ import '../App.css';
 
 class App extends Component {
 
+  storeLocally = (countdown, note) => {
+    let storage = {
+      countdown,
+      note
+    }
+
+    localStorage.setItem('storage', JSON.stringify(storage))
+  }
+
+  getStoredLocally = () => {
+    let stored = localStorage.getItem('storage');
+
+    if (stored === null){
+        return [];
+    } else {
+        return JSON.parse(stored);
+    }
+  }
+
+  storage = this.getStoredLocally();
+
   state = {
     optionMode: false,
-    countDownSetByUser: '',
-    noteSetByUser: ''
+    countDownSetByUser: this.storage.countdown,
+    noteSetByUser: this.storage.note
+  }
+
+  handleChange = (countDown, note) => {
+    // Reset old values with new ones in state
+    this.setState({countDownSetByUser: countDown, noteSetByUser: note});
+
+    /* Store them locally - next time user loads page
+    the state will instead be set to the localStorage */
+    this.storeLocally(countDown, note);
   }
 
   toggleOptionDisplay = () => {
     this.state.optionMode ? this.setState({optionMode: false}) : this.setState({optionMode: true});
-  }
-
-  handleChange = (date, note) => {
-    this.setState({countDownSetByUser: date, noteSetByUser: note});
   }
 
   render() {
