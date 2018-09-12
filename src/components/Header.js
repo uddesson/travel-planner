d5c08379175d7526a26c93de96f23fee75219652
Emma Uddesson <emma.uddesson@gmail.com>
2018-09-12 21:45:39 +0200

    Update structure of header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,32 +7,26 @@ import AnchorLink from "./AnchorLink";
 
 
 function Header(props){
+  return(
+    <div className="header">
+      <div className="u-margin-bottom">
+          <CurrentTime/>
+      </div>
 
-    return(
-        <div className="header">
-            <div className="u-margin-bottom">
-                <CurrentTime/>
-                <AnchorLink
-                    href="#options"
-                    className="u-right"
-                    onClick={ props.toggleOptionMode }
-                    content="Show Options"
-                />
-            </div>
+      <div className="container__inner">
+        <Title
+            size={1}
+            content="Your trip to Japan"
+            className="u-margin-bottom u-center"
+        />
 
-            <Title
-                size={1}
-                content="Your trip to Japan"
-                className="u-margin-bottom u-center"
-            />
-
-            <CountDown
-                toggleOptionMode={ props.toggleOptionMode }
-                countDownSetByUser={ props.countDownSetByUser }
-            />
-
+        <CountDown
+            toggleOptionMode={ props.toggleOptionMode }
+            countDownSetByUser={ props.countDownSetByUser }
+        />
         </div>
-    );
+    </div>
+  );
 }
 
 Header.propTypes = {
